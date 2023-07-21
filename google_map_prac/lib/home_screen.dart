3c407f506deb43720d9e621c4ae9eb4f0aa81bd7
Muhammad Ibrar <muhammad49ibrar@gmail.com>
2023-07-21 16:39:18 +0500

    google map practice added multiple markers and current location

---
+++ google_map_prac/lib/home_screen.dart
@@ -0,0 +1,61 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  Completer<GoogleMapController> _controller = Completer();
+  static final CameraPosition _kGooglePlex =CameraPosition(
+      target:LatLng(33.86338870354975, 71.56588407172485),
+    zoom: 14.4746,
+
+  );
+
+  List<Marker> _marker = [];
+  final List<Marker> _list = [
+    const Marker(
+        markerId: MarkerId('1'),
+      infoWindow: InfoWindow(
+        title: 'My Location'
+      ),
+      position: LatLng(33.86338870354975, 71.56588407172485),
+
+    ),
+    const Marker(
+      markerId: MarkerId('2'),
+      infoWindow: InfoWindow(
+          title: 'Marker 2'
+      ),
+      position: LatLng(32.86338870354975, 71.56588407172485),
+
+    )
+  ];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _marker.addAll(_list);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: _kGooglePlex,
+        markers: Set<Marker>.of(_marker),
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+
+      ),
+    );
+  }
+}
